757b989eb8bcdd2650974cfbc3156ee7a33a30c1
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-12-05 10:18:43 -0600

    Create Logic 2: blackjack.java

---
+++ Logic 2: blackjack.java
@@ -0,0 +1,16 @@
+/*Given 2 int values greater than 0, 
+return whichever value is nearest to 21 without going over. 
+Return 0 if they both go over.*/
+
+public int blackjack(int a, int b) {
+  if(a>21&&b>21){
+    return 0;
+  }else if(a>21){
+    return b;
+  }else if(b>21){
+    return a;
+  }else if(a>b){
+    return a;
+  }
+  return b;
+}
