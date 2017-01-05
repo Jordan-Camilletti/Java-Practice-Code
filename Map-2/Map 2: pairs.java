4d8903a8dde45627fa1d8f861c2861f8aa990fc6
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-05 11:14:55 -0600

    Update Map 2: pairs.java

--- Map-2/Map 2: pairs.java
+++ Map-2/Map 2: pairs.java
@@ -1,3 +1,7 @@
+/*Given an array of non-empty strings, 
+create and return a Map<String, String> as follows:
+for each string add its first character as a key with its last character as the value.*/
+
 public Map<String, String> pairs(String[] strings) {
   Map<String, String> map = new HashMap<String, String>();
   for(int x=0;x<strings.length;x++){
