e5d763c27b6f7430701c4ac40b001e13fc15e792
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-17 13:45:42 -0600

    Update and rename AP 1: f.java to AP 1: wordsWithout.java

--- AP-1/AP 1: wordsWithout.java
+++ AP-1/AP 1: wordsWithout.java
@@ -1,3 +1,7 @@
+/*Given an array of strings, return a new array without the strings that are equal to the target string.
+One approach is to count the occurrences of the target string, 
+make a new array of the correct length, and then copy over the correct strings.*/
+
 public String[] wordsWithout(String[] words, String target){
   String str="";
   for(int x=0;x<words.length;x++){
