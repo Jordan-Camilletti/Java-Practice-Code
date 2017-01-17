d9692d96ab26c63d9665e618ef61fa8036911cb1
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-17 13:45:05 -0600

    Update AP 1: f.java

--- AP-1/AP 1: f.java
+++ AP-1/AP 1: f.java
@@ -2,9 +2,9 @@ public String[] wordsWithout(String[] words, String target){
   String str="";
   for(int x=0;x<words.length;x++){
     if(words[x].equals(target)==false){
-      str=str+words[x];
+      str=str+" "+words[x];
     }
   }
-  String wrd[]=str.split("");
+  String wrd[]=(str.substring(1,str.length())).split(" ");
   return wrd;
 }
