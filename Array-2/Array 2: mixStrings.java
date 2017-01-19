a3e3eab16681129fb897d3729d003c0aa98f48a1
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-19 11:59:50 -0600

    Update Array 2: mixStrings.java

--- Array-2/Array 2: mixStrings.java
+++ Array-2/Array 2: mixStrings.java
@@ -1,3 +1,6 @@
+/*Given two strings, a and b, 
+create a bigger string made of the first char of a, the first char of b, the second char of a, the second char of b, and so on.
+Any leftover chars go at the end of the result.*/
 public String mixString(String a, String b) {
   String wrd="";
   String aArr[]=a.split("");
