fc64c9f7a31b45a5d86bd280ce775984f2f94f9d
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-22 22:11:01 -0600

    Update AP 1: scoresSpecial.java

--- AP-1/AP 1: scoresSpecial.java
+++ AP-1/AP 1: scoresSpecial.java
@@ -3,3 +3,15 @@ A "special" score is one which is a multiple of 10, such as 40 or 90.
 Return the sum of largest special score in A and the largest special score in B. To practice decomposition,
 write a separate helper method which finds the largest special score in an array. 
 Write your helper method after your scoresSpecial() method in the JavaBat text area.*/
+
+public int biggest(int[] arr){
+  int top=0;
+  for(int x=0;x<arr.length;x++){
+    if(arr[x]>top&&arr[x]%10==0) top=arr[x];
+  }
+  return top;
+}
+
+public int scoresSpecial(int[] a, int[] b){
+  return biggest(a)+biggest(b);
+}
