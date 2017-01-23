b756bbdc0af5ac397a27ffe271bfaf8548d0be26
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-22 22:10:51 -0600

    Update AP 1: scoresSpecial.java

--- AP-1/AP 1: scoresSpecial.java
+++ AP-1/AP 1: scoresSpecial.java
@@ -1,1 +1,5 @@
-
+/*Given two arrays, A and B, of non-negative int scores.
+A "special" score is one which is a multiple of 10, such as 40 or 90. 
+Return the sum of largest special score in A and the largest special score in B. To practice decomposition,
+write a separate helper method which finds the largest special score in an array. 
+Write your helper method after your scoresSpecial() method in the JavaBat text area.*/
