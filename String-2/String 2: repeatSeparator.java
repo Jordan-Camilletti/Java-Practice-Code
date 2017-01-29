779cb40d3a08c0ff00a8c416a80a29ed77252c3a
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-28 23:04:42 -0600

    Update and rename String 2: to String 2: repeatSeparator.java

--- String-2/String 2: repeatSeparator.java
+++ String-2/String 2: repeatSeparator.java
@@ -1,3 +1,7 @@
+/*Given two strings, word and a separator sep,
+return a big string made of count occurrences of the word, 
+separated by the separator string.*/
+
 public String repeatSeparator(String word, String sep, int count) {
   String wrd="";
   for(int x=0;x<(count*2)-1;x++){
