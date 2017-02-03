7f9d56e698f756097be669faee27fa74181ae6c9
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-02-03 15:39:33 -0600

    Update String 3- withoutString.java

--- String-3/String 3- withoutString.java
+++ String-3/String 3- withoutString.java
@@ -1,7 +1,7 @@
-Given two strings, base and remove, 
+/*Given two strings, base and remove, 
 return a version of the base string where all instances of the remove string have been removed (not case sensitive). 
 You may assume that the remove string is length 1 or more.
-Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
+Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".*/
 
 public String withoutString(String base, String remove) {
   return base.replaceAll(("(?i)"+remove), "");
