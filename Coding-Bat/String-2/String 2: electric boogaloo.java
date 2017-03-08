7d1843839c3426ec730f373472b90ea110f588b4
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-03-08 13:58:48 -0600

    Update String 2: electric boogaloo.java

--- Coding-Bat/String-2/String 2: electric boogaloo.java
+++ Coding-Bat/String-2/String 2: electric boogaloo.java
@@ -1,10 +1,20 @@
-public boolean xyzMiddle(String str) {
+public String getSandwich(String str){
   String[] arr=str.split("");
-  if(arr.length<=3||str.equals("xyz")==false) return false;
-  for(int x=0;x<arr.length-2;x++){
-    if(arr[x].equals("x")&&arr[x+1].equals("y")&&arr[x+2].equals("z")){
-      if(str.substring(0,x).length()==str.substring(x,str.length()).length()) return true;
+  int lastBread=-1;
+  int firstBread=-1;
+  if(str.length()<=10) return "";
+  for(int x=0;x<arr.length-4;x++){
+    if(arr[x].equals("b")&&arr[x+1].equals("r")&&arr[x+2].equals("e")&&arr[x+3].equals("a")&&arr[x+4].equals("d")){
+      firstBread=x+4;
+      break;
     }
   }
-  return false;
+  for(int x=arr.length-1;x>=0;x--){
+    if(arr[x].equals("d")&&arr[x-1].equals("a")&&arr[x-2].equals("e")&&arr[x-3].equals("r")&&arr[x-4].equals("b")){
+      lastBread=x-4;
+      break;
+    }
+  }
+  if(lastBread==firstBread) return "";
+  return 
 }
