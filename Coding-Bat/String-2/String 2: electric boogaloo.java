45bf56e9440c52b5725224521e20adb146b2264f
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-03-08 12:49:13 -0600

    Update String 2: electric boogaloo.java

--- Coding-Bat/String-2/String 2: electric boogaloo.java
+++ Coding-Bat/String-2/String 2: electric boogaloo.java
@@ -2,8 +2,8 @@ public boolean xyzMiddle(String str) {
   String[] arr=str.split("");
   if(arr.length<=3||str.equals("xyz")==false) return false;
   for(int x=0;x<arr.length-2;x++){
-    if(arr[x].equals("x")&&arr[x+1].equals&&arr[x+2]){
-      
+    if(arr[x].equals("x")&&arr[x+1].equals("y")&&arr[x+2].equals("z")){
+      if(str.substring(0,x).length()==str.substring(x,str.length()).length()) return true;
     }
   }
   return false;
