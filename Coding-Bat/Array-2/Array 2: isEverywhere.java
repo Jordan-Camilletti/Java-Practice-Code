6264a6fed33643f671e7d8558244d575d0058248
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-03-11 12:15:11 -0600

    Update and rename Array-2/Array 2: isEverywhere.java to Coding-Bat/Array-2/Array 2: isEverywhere.java

--- Coding-Bat/Array-2/Array 2: isEverywhere.java
+++ Coding-Bat/Array-2/Array 2: isEverywhere.java
@@ -1,5 +1,5 @@
 /*We'll say that a value is "everywhere" in an array if for every pair of adjacent elements in the array, 
-at least one of the pair is that value. Return true if the given value is everywhere in the array./*
+at least one of the pair is that value. Return true if the given value is everywhere in the array.*/
 
 public boolean isEverywhere(int[] nums, int val) {
   boolean t1=true;
