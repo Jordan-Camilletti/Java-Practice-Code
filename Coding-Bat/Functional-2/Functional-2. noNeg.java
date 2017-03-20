4d2730417280edcbbf59e40a226ea870e9813def
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-03-20 08:39:19 -0500

    Update Functional-2. noNeg.java

--- Coding-Bat/Functional-2/Functional-2. noNeg.java
+++ Coding-Bat/Functional-2/Functional-2. noNeg.java
@@ -1,5 +1,6 @@
-/*^*/
+/*Given a list of integers, return a list of the integers, omitting any that are less than 0.*/
 
 public List<Integer> noNeg(List<Integer> nums) {
   nums.removeIf(n->n<0);
+  return nums;
 }
