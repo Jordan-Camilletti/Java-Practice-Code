f1d37ad325eea8d054db9f5cc3088f7645559a56
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-03-21 12:09:46 -0500

    Update and rename Functional-2: to Functional-2: noTeen.java

---
+++ Coding-Bat/Functional-2/Functional-2: noTeen.java
@@ -0,0 +1,6 @@
+/*Given a list of integers, return a list of those numbers, omitting any that are between 13 and 19 inclusive.*/
+
+public List<Integer> noTeen(List<Integer> nums) {
+  nums.removeIf(n->(n>=13&&n<=19));
+  return nums;
+}
