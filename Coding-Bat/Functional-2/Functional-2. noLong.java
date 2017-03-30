2d138062d624fca4f572d09819f05afab43ab6ef
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-03-30 11:38:08 -0500

    Update and rename Functional-2. trains go choo choo to Functional-2. noLong.java

--- Coding-Bat/Functional-2/Functional-2. noLong.java
+++ Coding-Bat/Functional-2/Functional-2. noLong.java
@@ -1,4 +1,4 @@
-
+//Given a list of strings, return a list of the strings, omitting any string length 4 or more.
 
 public List<String> noLong(List<String> strings) {
   strings.removeIf(n->n.length()>=4);
