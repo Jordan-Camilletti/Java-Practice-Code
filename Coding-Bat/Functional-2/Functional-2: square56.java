a383afbc42ec61b18460588c5201720eec6409a4
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-03-30 11:49:27 -0500

    Update and rename Functional-2: choo choo bugaboo to Functional-2: square56.java

---
+++ Coding-Bat/Functional-2/Functional-2: square56.java
@@ -0,0 +1,8 @@
+//Given a list of integers, return a list of those numbers squared and the product added to 10, 
+omitting any of the resulting numbers that end in 5 or 6.
+
+public List<Integer> square56(List<Integer> nums){
+  nums.replaceAll(n->(n*n)+10);
+  nums.removeIf(n->n%10==5||n%10==6);
+  return nums;
+}
