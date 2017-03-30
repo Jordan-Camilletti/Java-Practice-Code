ed2644ee0670a7c14052ee23683bbc378b2a9532
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-03-30 11:50:33 -0500

    Update Functional-2: square56.java

--- Coding-Bat/Functional-2/Functional-2: square56.java
+++ Coding-Bat/Functional-2/Functional-2: square56.java
@@ -1,5 +1,5 @@
-//Given a list of integers, return a list of those numbers squared and the product added to 10, 
-omitting any of the resulting numbers that end in 5 or 6.
+/*Given a list of integers, return a list of those numbers squared and the product added to 10, 
+omitting any of the resulting numbers that end in 5 or 6.*/
 
 public List<Integer> square56(List<Integer> nums){
   nums.replaceAll(n->(n*n)+10);
