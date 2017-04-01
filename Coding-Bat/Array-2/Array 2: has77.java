627cf6b0e4af45797708f3bf3306501ba815e2b2
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-04-01 12:56:00 -0500

    Update and rename Array 2" fmaolwef to Array 2: has77.java

--- Coding-Bat/Array-2/Array 2: has77.java
+++ Coding-Bat/Array-2/Array 2: has77.java
@@ -1,3 +1,5 @@
+/*Given an array of ints, return true if the array contains two 7's next to each other, 
+or there are two 7's separated by one element, such as with {7, 1, 7}.*/
 
 public boolean has77(int[] nums) {
   if(nums.length>=2 && nums[nums.length-2]==7&&nums[nums.length-1]==7) return true;
