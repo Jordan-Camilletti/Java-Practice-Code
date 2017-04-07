4025f0ebb26f3d657bc95207457fcc9a261d1b2a
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-04-07 14:12:48 -0500

    Update Array 2: haveThree.java

--- Coding-Bat/Array-2/Array 2: haveThree.java
+++ Coding-Bat/Array-2/Array 2: haveThree.java
@@ -2,7 +2,8 @@
 
 public boolean haveThree(int[] nums){
   int count=0;
+  if(nums.length>=2 && nums[nums.length-1]==3&&nums[nums.length-2]!=3) count=1;
   for(int x=0;x<nums.length-1;x++){
     if(nums[x]==3&&nums[x+1]!=3) count++;
-  }return (count==3||(count==2&&nums[nums.length-1]==3&&nums[nums.length-2]!=3));
+  }return (count==3);
 }
