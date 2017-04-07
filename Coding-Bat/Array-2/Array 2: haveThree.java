3c2b82081033797f13bd0aac444cfc2ff6ad5ea4
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-04-07 14:13:18 -0500

    Update Array 2: haveThree.java

--- Coding-Bat/Array-2/Array 2: haveThree.java
+++ Coding-Bat/Array-2/Array 2: haveThree.java
@@ -1,4 +1,6 @@
-/**/
+/*Given an array of ints, 
+return true if the value 3 appears in the array exactly 3 times,
+and no 3's are next to each other.*/
 
 public boolean haveThree(int[] nums){
   int count=0;
