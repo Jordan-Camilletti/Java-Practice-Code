71117b2cdb36857ffcb5e8bed6c222e734593639
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-04-12 09:32:14 -0500

    Update and rename Recursion 1: don't know what to put for this one to Recursion 1: powerN.java

---
+++ Coding-Bat/Recursion-1/Recursion 1: powerN.java
@@ -0,0 +1,8 @@
+/*Given base and n that are both 1 or more,
+compute recursively (no loops) the value of base to the n power,
+so powerN(3, 2) is 9 (3 squared).*/
+
+public int powerN(int base, int n) {
+  if(n==1) return base;
+  return(powerN(base,n-1)*base);
+}
