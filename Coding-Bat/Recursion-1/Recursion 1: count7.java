3fc9d352ee436aea4593b7edd12488126d180d63
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-04-12 09:25:24 -0500

    Update and rename Recursion 1: I like to eat bun bun to Recursion 1: count7.java

--- Coding-Bat/Recursion-1/Recursion 1: count7.java
+++ Coding-Bat/Recursion-1/Recursion 1: count7.java
@@ -1,3 +1,5 @@
+/*Given a non-negative int n, return the count of the occurrences of 7 as a digit, so for example 717 yields 2. (no loops). */
+
 public int count7(int n) {
   if(n==7) return 1;
   if(n<10) return 0;
