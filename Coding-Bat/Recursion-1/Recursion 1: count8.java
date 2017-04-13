de713ced9635d2b2fd0754266be4b663cd0735ac
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-04-13 08:27:06 -0500

    Update and rename Recursion 1: I ain't done to Recursion 1: count8.java

---
+++ Coding-Bat/Recursion-1/Recursion 1: count8.java
@@ -0,0 +1,11 @@
+/*Given a non-negative int n, 
+compute recursively (no loops) the count of the occurrences of 8 as a digit,
+except that an 8 with another 8 immediately to its left counts double, so 8818 yields 4. */
+
+public int count8(int n){
+  if(n==8) return 1;
+  if(n<10) return 0;
+  if(n%10==8 && (n%100)/10==8) return (2+count8(n/10));
+  if(n%10==8) return (1+count8(n/10));
+  return (count8(n/10));
+}
