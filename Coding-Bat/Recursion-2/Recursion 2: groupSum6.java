7ea35d2459a4f5bdd0abdb57e7c12e65de14fdf4
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-04-18 08:21:46 -0500

    Update and rename Recursion 2: I gotta use the loo to Recursion 2: groupSum6.java

--- Coding-Bat/Recursion-2/Recursion 2: groupSum6.java
+++ Coding-Bat/Recursion-2/Recursion 2: groupSum6.java
@@ -1,5 +1,6 @@
-/*What ever are my names anymore?
-rawr Xd*/
+/*Given an array of ints, is it possible to choose a group of some of the ints, 
+beginning at the start index, such that the group sums to the given target? However, 
+with the additional constraint that all 6's must be chosen. (No loops needed.)*/
 
 public boolean groupSum6(int start, int[] nums, int target) {
   if(start>=nums.length) return(target==0);
