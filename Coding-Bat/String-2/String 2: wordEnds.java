2f48885aec0e262f783d39995375da58cebfef6f
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-05-01 08:37:58 -0500

    Update and rename STRING 2: I AM HUMAN TOO to String 2: wordEnds.java

--- Coding-Bat/String-2/String 2: wordEnds.java
+++ Coding-Bat/String-2/String 2: wordEnds.java
@@ -1,4 +1,6 @@
-/**/
+/*Given a string and a non-empty word string, 
+return a string made of each char just before and just after every appearance of the word in the string. 
+Ignore cases where there is no char before or after the word, and a char may be included twice if it is between two words.*/
 
 public String wordEnds(String str, String word){
   String newWrd="";
