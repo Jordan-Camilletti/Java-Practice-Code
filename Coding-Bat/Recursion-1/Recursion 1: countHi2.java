599b5bf2d4b67499aa4d64e8aa2f2e50ee5a2912
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-05-07 22:35:07 -0500

    Update and rename Recursion 1: don't know one to Recursion 1: countHi2.java

--- Coding-Bat/Recursion-1/Recursion 1: countHi2.java
+++ Coding-Bat/Recursion-1/Recursion 1: countHi2.java
@@ -1,4 +1,5 @@
-/**/
+/*Given a string, compute recursively the number of times lowercase "hi" appears in the string, 
+however do not count "hi" that have an 'x' immedately before them*/
 
 public int countHi2(String str) {
   if(str.length()<2) return(0);
