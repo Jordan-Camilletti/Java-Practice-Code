ec957b373a238b4d269609730f8eed035ecb5026
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-05-16 14:17:48 -0500

    Update and rename String 2: I got a boo boo to String 2: zipZap.java

--- Coding-Bat/String-2/String 2: zipZap.java
+++ Coding-Bat/String-2/String 2: zipZap.java
@@ -1,4 +1,5 @@
-/**/
+/*Look for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'.
+Return a string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".*/
 
 public String zipZap(String str) {
   for(int x=0;x<str.length()-2;x++){
