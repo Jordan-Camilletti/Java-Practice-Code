ce03e0520ef3b3c31926bd9237a0454ae6ec35d8
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-05-21 23:44:53 -0500

    Update and rename String 2: I'm glad I'm through to String 2: xyzMiddle.java

--- Coding-Bat/String-2/String 2: xyzMiddle.java
+++ Coding-Bat/String-2/String 2: xyzMiddle.java
@@ -1,4 +1,6 @@
-/**/
+/*Given a string, does "xyz" appear in the middle of the string? 
+To define middle, we'll say that the number of chars to the left and right of the "xyz" must differ by at most one. 
+This problem is harder than it looks.*/
 
 public boolean xyzMiddle(String str){
   String arr[]=new String[2];
