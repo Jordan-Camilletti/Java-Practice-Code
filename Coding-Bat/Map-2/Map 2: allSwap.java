6189988656a870fed043cdf4d7ed5fa1017394a3
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-05-26 15:38:38 -0500

    Update and rename Map 2: looloo to Map 2: allSwap.java

--- Coding-Bat/Map-2/Map 2: allSwap.java
+++ Coding-Bat/Map-2/Map 2: allSwap.java
@@ -1,3 +1,9 @@
+/*We'll say that 2 strings "match" if they are non-empty and their first chars are the same. 
+Loop over and then return the given array of non-empty strings as follows:
+if a string matches an earlier string in the array, swap the 2 strings in the array.
+When a position in the array has been swapped, it no longer matches anything.
+Using a map, this can be solved making just one pass over the array.*/
+
 public String[] allSwap(String[] strings){
   Map<String, String> map=new HashMap<String, String>();
   String thingy;
