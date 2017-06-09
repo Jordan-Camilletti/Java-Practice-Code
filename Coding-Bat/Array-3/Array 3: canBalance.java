319576589bfa699bcef10c6e3340c5df5ffc49a6
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-09 16:02:11 -0500

    Create Array 3: canBalance.java

--- Coding-Bat/Array-3/Array 3: canBalance.java
+++ Coding-Bat/Array-3/Array 3: canBalance.java
@@ -1,4 +1,6 @@
-/*Given a non-empty array, return true if there is a place to split the array so that the sum of the numbers on one side is equal to the sum of the numbers on the other side.*/
+/*Given a non-empty array,
+return true if there is a place to split the array 
+so that the sum of the numbers on one side is equal to the sum of the numbers on the other side.*/
 
 public boolean canBalance(int[] nums) {
   int tot1=0,tot2=0;
