a5fd0e5b85681f15841d790b0b3ed5591d70c286
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-18 00:19:19 -0500

    Update and rename String 3:3 gnirtS to String 3: gHappy.java

--- Coding-Bat/String-3/String 3: gHappy.java
+++ Coding-Bat/String-3/String 3: gHappy.java
@@ -1,4 +1,5 @@
-/**/
+/*We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
+Return true if all the g's in the given string are happy.*/
 
 public boolean gHappy(String str) {
   String arr[]=str.split("");
