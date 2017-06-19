297a0dd4f85a70fa90ff8676b327ef03e0ab2527
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-19 18:22:43 -0500

    Update and rename Array 3:3 yarrA to Array 3: squareUp.java

--- Coding-Bat/Array-3/Array 3: squareUp.java
+++ Coding-Bat/Array-3/Array 3: squareUp.java
@@ -1,4 +1,5 @@
-/**/
+/*Given n>=0, create an array length n*n with the following pattern, shown here for n=3 : 
+{0, 0, 1,    0, 2, 1,    3, 2, 1} (spaces added to show the 3 groups).*/
 
 public int[] squareUp(int n) {
   int num=0;
