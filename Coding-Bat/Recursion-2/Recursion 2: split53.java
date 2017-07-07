7f94d19adf7919ea9cc15ee3b1b07963d3ea49dc
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-07-06 20:24:26 -0500

    Update and rename Recursion 2::: to Recursion 2: split53.java

--- Coding-Bat/Recursion-2/Recursion 2: split53.java
+++ Coding-Bat/Recursion-2/Recursion 2: split53.java
@@ -1,4 +1,7 @@
-/**/
+/*Given an array of ints, is it possible to divide the ints into two groups,
+so that the sum of the two groups is the same, with these constraints: 
+all the values that are multiple of 5 must be in one group,
+and all the values that are a multiple of 3 (and not a multiple of 5) must be in the other.*/
 
 public boolean stuff(int[] nums, int g1, int g2, int c){
   if(c>=nums.length) return(g1==g2);
