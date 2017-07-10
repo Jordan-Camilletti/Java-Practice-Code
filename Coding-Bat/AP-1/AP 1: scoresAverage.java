8384f6b20d126fa56edae247c6a36ea24fcaee7a
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-07-10 00:36:59 -0500

    Update and rename AP 1: efokaed to AP 1: scoresAverage.java

--- Coding-Bat/AP-1/AP 1: scoresAverage.java
+++ Coding-Bat/AP-1/AP 1: scoresAverage.java
@@ -1,4 +1,6 @@
-/**/
+/*Given an array of scores, compute the int average of the first half and the second half, 
+and return whichever is larger. We'll say that the second half begins at index length/2. 
+The array length will be at least 2. To practice decomposition, write a separate helper method int average.*/
 
 public int average(int[] scores, int s, int e){
   int score=0;
