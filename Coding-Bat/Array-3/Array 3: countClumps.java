6ce45781e84762f674d18f6f27870e64c19bc458
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-07-13 20:00:35 -0500

    Update and rename Array 3:3 yarrA to Array 3: countClumps.java

--- Coding-Bat/Array-3/Array 3: countClumps.java
+++ Coding-Bat/Array-3/Array 3: countClumps.java
@@ -1,4 +1,5 @@
-/**/
+/*Say that a "clump" in an array is a series of 2 or more adjacent elements of the same value.
+Return the number of clumps in the given array.*/
 
 public int countClumps(int[] nums) {
   int clumps=0;
