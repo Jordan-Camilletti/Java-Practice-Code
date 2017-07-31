cc627daed2873dd0d7b433601ba411098246e880
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-07-30 21:14:40 -0500

    Update and rename AP 1: one and done to AP 1: commonTwo.java

--- Coding-Bat/AP-1/AP 1: commonTwo.java
+++ Coding-Bat/AP-1/AP 1: commonTwo.java
@@ -1,4 +1,7 @@
-/**/
+/*Start with two arrays of strings, a and b, each in alphabetical order, possibly with duplicates. 
+Return the count of the number of strings which appear in both arrays. 
+The best "linear" solution makes a single pass over both arrays,
+taking advantage of the fact that they are in alphabetical order.*/
 
 public int commonTwo(String[] a, String[] b) {
   int aNum=0,bNum=0,diff=0;
