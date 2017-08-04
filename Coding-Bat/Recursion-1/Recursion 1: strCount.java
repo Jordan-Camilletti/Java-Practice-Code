85dfb71e8c1cef8e122276111791ab99123c7bf0
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-08-03 22:07:41 -0500

    Update Recursion 1: strCount.java

--- Coding-Bat/Recursion-1/Recursion 1: strCount.java
+++ Coding-Bat/Recursion-1/Recursion 1: strCount.java
@@ -1,4 +1,5 @@
-/**/
+/*Given a string and a non-empty substring sub, 
+compute recursively the number of times that sub appears in the string, without the sub strings overlapping.*/
 
 public int strCount(String str, String sub) {
   if(str.length()<sub.length()) return 0;
