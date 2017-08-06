eb4addce0ea5c6961117cd70784a9d61d665d19c
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-08-06 18:38:47 -0500

    Update String 3: sameEnds.java

--- Coding-Bat/String-3/String 3: sameEnds.java
+++ Coding-Bat/String-3/String 3: sameEnds.java
@@ -1,4 +1,4 @@
-/**/
+/*Given a string, return the longest substring that appears at both the beginning and end of the string without overlapping.*/
 
 public String sameEnds(String str){
   for(int x=str.length()/2;x>0;x--){
