b07e09e5bd1634b9c43ffe047c88f4ed428204e9
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-08-13 21:49:27 -0500

    Update and rename Array 3: ae0feee to Array 3: sumNumbers.java

--- Coding-Bat/Array-3/Array 3: sumNumbers.java
+++ Coding-Bat/Array-3/Array 3: sumNumbers.java
@@ -1,4 +1,5 @@
-/**/
+/*Given a string, return the sum of the numbers appearing in the string, 
+ignoring all other characters. A number is a series of 1 or more digit chars in a row.*/
 
 public int sumNumbers(String str) {
   int tot=0;
