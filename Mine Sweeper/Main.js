72cd7e2b498f63e3a8d6e4cb96915cc61bd12e18
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-08-23 00:18:42 -0500

    Update Main.js

--- Mine Sweeper/Main.js
+++ Mine Sweeper/Main.js
@@ -1,2 +1,17 @@
 /**/
 
+package main;
+
+import javax.*;
+import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main {
+	public static void main(String[] args){
+		JFrame frame=new JFrame("XD");
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(650, 650);
+	}
+}
