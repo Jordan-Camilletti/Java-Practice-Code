def938f02c55fefd5f583504b0009f908d308364
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-08-23 23:18:40 -0500

    Update Main.java

--- Mine Sweeper/Main.java
+++ Mine Sweeper/Main.java
@@ -2,13 +2,28 @@
 
 package main;
 
-import javax.swing.*;
+import java.util.Arrays;
+//import javax.swing.*;
 
 public class Main {
 	public static void main(String[] args){
-		JFrame frame=new JFrame("XD");
+		boolean[][] revieled=new boolean[12][12];
+		int[][] field=new int[12][12];
+		Arrays.fill(revieled, false);
+		Arrays.fill(field, 0);
+		for(int y=0;y<12;y++){
+			for(int x=0;x<12;x++){
+				if(revieled[y][x]){
+					System.out.print(field[y][x]);
+				}else{
+					System.out.print("X");
+				}
+			}
+			System.out.print("\n");
+		}
+		/*JFrame frame=new JFrame("XD");
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(650, 650);
+		frame.setSize(650, 650);*/
 	}
 }
