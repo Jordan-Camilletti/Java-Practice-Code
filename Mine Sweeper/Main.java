8407d87007ca1ae2e097eb45e08a2f70730cac53
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-08-24 18:37:34 -0500

    Update Main.java

--- Mine Sweeper/Main.java
+++ Mine Sweeper/Main.java
@@ -8,8 +8,8 @@ import java.util.Random;
 //import javax.swing.*;
 
 public class Main {
-	public static int reveal(){//Revealing a spot on the field
-		return 0;
+	public static int[][] revealZero(int[][] field, int choiceX, int choiceY){//Revealing a spot on the field
+		return field;
 	}
 	
 	public static void main(String[] args){
@@ -19,32 +19,40 @@ public class Main {
 		int[][] field=new int[yLen][xLen];
 		boolean[][] revealed=new boolean[yLen][xLen];
 		for(int y=0;y<yLen;y++){//Creating the field
-			Arrays.fill(revealed[y], true);//Change this to false later
+			Arrays.fill(revealed[y], false);//Change this to false later
 			Arrays.fill(field[y], 0);
 		}
 		for(int m=0;m<(xLen+yLen)/2;m++){//Adding mines, 9=mine
 			rndX=rnd.nextInt(12);
 			rndY=rnd.nextInt(12);
 			field[rndY][rndX]=9;
-			for(int y=0;y<3;y++){
-				for(int x=0;x<3;x++){
-					field[y][x]+=1;
+			for(int y=-1;y<=1;y++){
+				for(int x=-1;x<=1;x++){
+					try{
+						field[rndY+y][rndX+x]+=1;
+					}catch(java.lang.ArrayIndexOutOfBoundsException e){
+						
+					}
 				}
 			}
 		}
-		while(reveal()!=9){
-			choiceX=sc.nextInt();
-			choiceY=sc.nextInt();
-			revealed[choiceY][choiceX]=true;
+		while(field[choiceY][choiceX]!=9&&revealed[choiceY][choiceX]==true){
 			for(int y=0;y<12;y++){
 				for(int x=0;x<12;x++){
 					if(revealed[y][x]){
+						if(field[y][x]>9) field[y][x]=9;
 						System.out.print(field[y][x]);
 					}else{
 						System.out.print("X");
 					}
 				}
 				System.out.print("\n");
+			}
+			choiceX=sc.nextInt();
+			choiceY=sc.nextInt();
+			revealed[choiceY][choiceX]=true;
+			if(field[choiceY][choiceX]==0){
+				field=revealZero(field,choiceX,choiceY);
 			}
 		}
 		/*JFrame frame=new JFrame("XD");
