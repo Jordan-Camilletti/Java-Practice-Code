2d63cb0b78d47c6bac6f088fa2f429f89198af06
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-08-28 15:22:13 -0500

    Update Mine Sweeper.java

--- Mine Sweeper/Mine Sweeper.java
+++ Mine Sweeper/Mine Sweeper.java
@@ -7,16 +7,20 @@ import java.util.*;
 import javax.swing.*;
 
 public class MineSweeper {
+        /*public static int[][] revealZero(int[][] field, int[][] revealed){
+            
+            return revealed;
+        }*/
 	public static int[][] reveal(int[][] field, int[][] revealed, int choiceX, int choiceY, String flag, int xLen, int yLen){//Revealing a spot on the field
 		if(flag.equals(" R")){
 			if(field[choiceY][choiceX]==0&&revealed[choiceY][choiceX]==0){
 				for(int y=-1;y<=1;y++){
 					for(int x=-1;x<=1;x++){
-						if(x!=0||y!=0){
+						if((x==)&&(y==)){
 							try{
 								revealed[choiceY+y][choiceX+x]=1;
-								//revealed=reveal(field,revealed,choiceX+x,choiceY+y,flag,xLen,yLen);
-							}catch(java.lang.ArrayIndexOutOfBoundsException e){}		
+                                                                revealed=reveal(field,revealed,choiceX+x,choiceY+y,flag,xLen,yLen);
+                                                        }catch(java.lang.ArrayIndexOutOfBoundsException e){}
 						}
 					}
 				}
@@ -45,7 +49,7 @@ public class MineSweeper {
 			Arrays.fill(revealed[y], 0);
 			Arrays.fill(field[y], 0);
 		}
-		for(int m=0;m<(xLen+yLen)/2;m++){//Adding mines, 9=mine
+		for(int m=0;m<0/*(xLen+yLen)/2*/;m++){//Adding mines, 9=mine
 			rndX=rnd.nextInt(xLen-1);
 			rndY=rnd.nextInt(yLen-1);
 			field[rndY][rndX]=9;
