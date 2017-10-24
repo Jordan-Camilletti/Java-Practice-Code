8b6a785bb0ccf8d6ada3f1e1dd64ea816a812833
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-10-24 14:26:27 -0500

    Update Array 2: notAlone.java

--- Coding-Bat/Array-2/Array 2: notAlone.java
+++ Coding-Bat/Array-2/Array 2: notAlone.java
@@ -1,4 +1,5 @@
-/*We'll say that an element in an array is "alone" if there are values before and after it, and those values are different from it. Return a version of the given array where every instance of the given value which is alone is replaced by whichever value to its left or right is larger.*/
+/*We'll say that an element in an array is "alone" if there are values before and after it,and those values are different from it.
+Return a version of the given array where every instance of the given value which is alone is replaced by whichever value to its left or right is larger.*/
 
 public int[] notAlone(int[] nums, int val) {
   if(nums.length<=1) return nums;
