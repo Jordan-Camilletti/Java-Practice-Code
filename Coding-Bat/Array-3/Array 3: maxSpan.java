60a7f80f81b5ae0aca4ed0209c887a7c0613904f
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-10-30 14:48:33 -0500

    Update and rename Array 3: normies? REEEEEEEEE to Array 3: maxSpan.java

--- Coding-Bat/Array-3/Array 3: maxSpan.java
+++ Coding-Bat/Array-3/Array 3: maxSpan.java
@@ -1,4 +1,7 @@
-/**/
+/*Consider the leftmost and righmost appearances of some value in an array.
+We'll say that the "span" is the number of elements between the two inclusive. A single value has a span of 1. 
+Returns the largest span found in the given array.
+(Efficiency is not a priority.)#me_irl*/
 
 public int maxSpan(int[] nums){
   int tot=0,leftMost=0,rightMost=0;
