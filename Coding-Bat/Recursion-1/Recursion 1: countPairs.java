077bd44097c45646f59f38d3d730fe25d6f198fd
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-11-22 19:50:01 -0600

    Update and rename Recursion 1: you have won to Recursion 1: countPairs.java

--- Coding-Bat/Recursion-1/Recursion 1: countPairs.java
+++ Coding-Bat/Recursion-1/Recursion 1: countPairs.java
@@ -1,4 +1,7 @@
-/**/
+/*We'll say that a "pair" in a string is two instances of a char separated by a char.
+So "AxA" the A's make a pair.
+Pair's can overlap, so "AxAxA" contains 3 pairs -- 2 for A and 1 for x.
+Recursively compute the number of pairs in the given string*/
 
 public int countPairs(String str) {
   if(str.length()<3){
