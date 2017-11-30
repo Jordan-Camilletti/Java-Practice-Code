54328d8cda9fee9509616c4b3535f91e0d68ec1d
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-11-30 13:31:14 -0600

    Update and rename Array 3: wheee! to Array 3: maxMirror.java

--- Coding-Bat/Array-3/Array 3: maxMirror.java
+++ Coding-Bat/Array-3/Array 3: maxMirror.java
@@ -1,4 +1,7 @@
-/**/
+/*We'll say that a "mirror" section in an array is a group of contiguous elements such that somewhere in the array, 
+the same group appears in reverse order.
+For example, the largest mirror section in {1, 2, 3, 8, 9, 3, 2, 1} is length 3 (the {1, 2, 3} part).
+Return the size of the largest mirror section found in the given array*/
 
 public boolean isIn(int[] nums, int startPos, int subLen){
   int[] slice=new int[subLen+1];
