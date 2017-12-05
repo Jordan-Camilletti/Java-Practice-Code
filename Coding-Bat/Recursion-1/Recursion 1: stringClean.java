7ca7292906b8827bab956d8a4a1e76fbe2011444
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-12-05 14:40:46 -0600

    Update and rename Recursion 1: I don't know what else rhymes with 1 to Recursion 1: stringClean.java

--- Coding-Bat/Recursion-1/Recursion 1: stringClean.java
+++ Coding-Bat/Recursion-1/Recursion 1: stringClean.java
@@ -1,9 +1,9 @@
-/*http://codingbat.com/prob/p104029*/
+/*Given a string, return recursively a "cleaned" string where adjacent chars that are the same have been reduced to a single char. 
+So "yyzzza" yields "yza".*/
 
 public String stringClean(String str){
   if(str.length()<2) return(str);
   if(str.length()==2 && str.substring(0,1).equals(str.substring(1,2))) return(str.substring(0,1));
-  //return("a");
   if(str.substring(0,1).equals(str.substring(1,2))){
     return(stringClean(str.substring(1,str.length())));
   }else{
