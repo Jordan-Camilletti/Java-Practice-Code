0527e360301c0fc918d84dbdfbedfea76e71b3b3
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-02-26 23:18:15 -0600

    Update Array 3: fix34.java

--- Coding-Bat/Array-3/Array 3: fix34.java
+++ Coding-Bat/Array-3/Array 3: fix34.java
@@ -3,7 +3,7 @@ but rearranged so that every 3 is immediately followed by a 4. Do not move the 3
 The array contains the same number of 3's and 4's, every 3 has a number after it that is not a 3,
 and a 3 appears in the array before any 4*/
 
-public int[] fix34(int[] nums) {
+public int[] fix34(int[] nums){
   for(int n=0;n<nums.length-1;n++){
     if(nums[n]==3){
       for(int n2=1;n2<nums.length;n2++){
