564e86df8e9063da685aacffa696807086f0bc7d
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-02-28 23:04:42 -0600

    Update Array 2: either24.java

--- Coding-Bat/Array-2/Array 2: either24.java
+++ Coding-Bat/Array-2/Array 2: either24.java
@@ -1,4 +1,4 @@
-/**/
+/*Given an array of ints, return true if the array contains a 2 next to a 2 or a 4 next to a 4, but not both.*/
 
 public boolean either24(int[] nums) {
   boolean a=false,b=false;
