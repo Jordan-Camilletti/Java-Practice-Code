3be3d5c9202a0bea3fe794f756545733b046d380
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-02-28 23:04:31 -0600

    Update Array 2: either24.java

--- Coding-Bat/Array-2/Array 2: either24.java
+++ Coding-Bat/Array-2/Array 2: either24.java
@@ -2,7 +2,7 @@
 
 public boolean either24(int[] nums) {
   boolean a=false,b=false;
-  for(int n=1;n<nums.length-1;n++){
+  for(int n=1;n<nums.length;n++){
     if(nums[n]==2 && nums[n-1]==2)
       a=true;
     if(nums[n]==4 && nums[n-1]==4)
