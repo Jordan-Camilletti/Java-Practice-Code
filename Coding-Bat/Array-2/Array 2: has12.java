78a2065cae5fa96623eb45b013698f43a3b2d644
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-03-02 23:05:08 -0600

    Update Array 2: has12.java

--- Coding-Bat/Array-2/Array 2: has12.java
+++ Coding-Bat/Array-2/Array 2: has12.java
@@ -1,7 +1,4 @@
-/*We'll say that an element in an array is "alone" if there are values before and after it,
-and those values are different from it.
-Return a version of the given array where every instance of the given value which is alone is replaced by 
-whichever value to its left or right is larger.*/
+/*Given an array of ints, return true if there is a 1 in the array with a 2 somewhere later in the array.*/
 
 public boolean has12(int[] nums) {
   for(int i=0;i<nums.length;i++){
