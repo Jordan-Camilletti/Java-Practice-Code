1dd535a5060b4dcc45a019179b2523bfcf1128b6
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-12-14 23:41:30 -0600

    Update Array 2: fizzBuzz.java

--- Coding-Bat/Array-2/Array 2: fizzBuzz.java
+++ Coding-Bat/Array-2/Array 2: fizzBuzz.java
@@ -2,13 +2,13 @@ public String[] fizzBuzz(int start, int end) {
   String[] nums=new String[end-start];
   for(int n=start;n<end;n++){
     if(n%3==0 && n%5==0){
-      
+      nums[n]="FizzBuzz";
     }else if(n%3==0){
-      
+      nums[n]="Fizz";
     }else if(n%5==0){
-      
+      nums[n]="Buzz";
     }else{
-      
+      nums[n]=n;  
     }
   }
   return(nums);
