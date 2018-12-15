4c60dd9deced97964e5d12cc243533f6c573b148
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-12-14 23:40:41 -0600

    Update and rename Array 2: FizzBuzz.java to Array 2: fizzBuzz.java

---
+++ Coding-Bat/Array-2/Array 2: fizzBuzz.java
@@ -0,0 +1,15 @@
+public String[] fizzBuzz(int start, int end) {
+  String[] nums=new String[end-start];
+  for(int n=start;n<end;n++){
+    if(n%3==0 && n%5==0){
+      
+    }else if(n%3==0){
+      
+    }else if(n%5==0){
+      
+    }else{
+      
+    }
+  }
+  return(nums);
+}
